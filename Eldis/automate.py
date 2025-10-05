bb78a12f8a01888527a867cad9a4595507baee82
Jan Werthmann <jan@werthmann.io>
2025-10-05 20:14:08 +0200

    Python Selenium erste Test für Eldis

--- Eldis/automate.py
+++ Eldis/automate.py
@@ -3,15 +3,25 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from getpass import getpass
 
+username = input("Benutzer: ")
+password = getpass("Passwort: ")
 
-service = Service(executable_path="D:\DEV\Selenium\Drivers\chromedriver-win64\chromedriver.exe")
+BetSys = os.name  
+print("Betriebssystem:", BetSys)
+
+service = Service(executable_path=r"D:\DEV\Selenium\Drivers\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+driver.get("https://einsatznachbearbeitung.bayern.de/login")
 
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf") 
-input_element.send_keys("Eldis Einsatznachbearbeitung")
+time.sleep(1)
 
-time.sleep(3)
+driver.find_element("id", "username").send_keys(username)
+driver.find_element("id", "password").send_keys(password)
+driver.find_element(By.XPATH, "/html/body/div/div[1]/section/main/div[1]/div[2]/form/div[3]/button").click()
+
+time.sleep(10)
 driver.quit()
