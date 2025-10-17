8ae22bf6b5f46f7e1dc4218bd9940229ada7dff1
Jan Werthmann <jan@werthmann.io>
2025-10-17 19:03:06 +0200

    Tabelle Ausgelesen und in Liste geschrieben

--- Eldis/automate.py
+++ Eldis/automate.py
@@ -11,6 +11,7 @@ username = input("Benutzer: ")
 password = getpass("Passwort: ")
 
 
+
 BetSys = os.name  
 print("Betriebssystem:", BetSys)
 if BetSys == "nt":  # Windows
@@ -30,12 +31,10 @@ driver.find_element(By.XPATH, "/html/body/div/div[1]/section/main/div[1]/div[2]/
 time.sleep(1)
 driver.find_element(By.XPATH, "/html/body/div/div/div/div/main/div/div/nav/ul/li/ul/li[1]/ul/li[1]/a/span").click() #Klicken auf Einsatzdaten
 time.sleep(1)
-"""
 driver.find_element(By.XPATH, "/html/body/div/div/div/div/main/div[3]/div/div/div[2]/div[1]/div[1]/button[2]").click()  #Klicken auf Anzahl Datensätze
 time.sleep(1)
 driver.find_element(By.XPATH, '//*[@id="page-length-100"]').click() #Klicken auf Radio Button 100 Einsätze
 time.sleep(3)
-"""
 schlagwort_zellen = driver.find_elements(By.CSS_SELECTOR, '[data-cell-field="schlagwort"]')
 
 
@@ -46,14 +45,22 @@ table1 = soup.find("table",{"class":"data-table"})
 table2 = soup.find('tbody')
 table3 = soup.find_all('tr')
 t = 1
-einsaetze = []
-for td in table3:
-    print(td.prettify())
-    einsaetze.append(td.get_text())
-for item in einsaetze:
+Einsatz = []
+Einsatzliste = []
+for tr in table3:
+    table4 = tr.find_all('td')
+    if len(Einsatz) != 0:
+        toprint = Einsatz[7],Einsatz[5],Einsatz[3],Einsatz[2]
+        print(toprint)
+    Einsatz = []
+    for td in table4:
+        Einsatz.append(td.get_text())
+    
+"""
+for item in Einsatzliste:
     print(t, item)
     t += 1
-"""
+
 # Die Texte extrahieren und in eine Liste schreiben
 schlagwort_liste = [zelle.text for zelle in schlagwort_zellen]
 
